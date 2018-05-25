bb577d8913c4a5fa0ade70d8e119d324b4956f0d
Dodoreptil <39606382+dodoreptil@users.noreply.github.com>
2018-05-24 23:51:56 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+ type="text/javascript">
+		window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			document.getElementById("botao").style.display = "block";
+		} else {
+			document.getElementById("botao").style.display = "none";
+		}
+	}
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
